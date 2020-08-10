045b48036f49c0060572d392b2820ab83751aa79
zbahbidi <johndoooe@laposte.net>
2020-08-10 16:48:15 +0200

    jeifj

--- marques.js
+++ marques.js
@@ -1,15 +1,8 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js"></script>
 
-<script>
 
-    new Glide('.glide', {
-        type: 'carousel',
+new Glide('.glide', {
+  type: 'carousel',
   startAt: 0,
   perView: 3,
   focusAt: 'center',
 }).mount()
-
-
-
-
-</script>
