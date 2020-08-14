85d00c3572fb545dfced6ccf609de647db8089c4
rob-nrkis <robh.dev@gmail.com>
2020-08-14 15:36:01 +0200

    create footer

--- header.js
+++ header.js
@@ -2,3 +2,9 @@
 //     var header = document.querySelector("header");
 //     header.classList.toggle("sticky", window.scrollY > 0);
 // })
+
+function PopupCentrer(page, largeur, hauteur, options) {
+    var top=(screen.height-hauteur)/2;
+    var left=(screen.width-largeur)/2;
+    window.open(page,"","top="+top+",left="+left+",width="+largeur+",height="+hauteur+","+options);
+  }
