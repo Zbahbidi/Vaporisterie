c25cba9d17d6279a51b0c773443212b109e4bec0
rob-nrkis <robh.dev@gmail.com>
2020-08-17 19:46:15 +0200

    test

--- header.js
+++ header.js
@@ -8,3 +8,11 @@ function PopupCentrer(page, largeur, hauteur, options) {
     var left=(screen.width-largeur)/2;
     window.open(page,"","top="+top+",left="+left+",width="+largeur+",height="+hauteur+","+options);
   }
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "header") {
+      x.className += " responsive";
+    } else {
+      x.className = "header";
+    }
+  }
